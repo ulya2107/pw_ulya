6a8d66fe155f8eee2d28cdba99739b0276979a84
ulya2107 <zifaaa2107@gmail.com>
2026-04-15 17:45:48 +0800

    commit pertamaku

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+$nama = "Abdull";
+$umur = 20;
+$tinggi = 170.5;
+$menikah = true;
+$hobi = ["membaca", "berenang","bermain gitar"];
+
+echo "Nama Saya $nama, Umur Saya $umur, tinggi saya $tinggi cm, status saya $menikah, hobi saya $hobi[0] , $hobi[1] , $hobi[2]";
+?>
