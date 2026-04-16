6fcd03fe35d05302991e4037dff6ecde130e9081
ulya2107 <zifaaa2107@gmail.com>
2026-04-16 08:40:10 +0800

    commit keduaku

--- index.php
+++ index.php
@@ -6,4 +6,36 @@ $menikah = true;
 $hobi = ["membaca", "berenang","bermain gitar"];
 
 echo "Nama Saya $nama, Umur Saya $umur, tinggi saya $tinggi cm, status saya $menikah, hobi saya $hobi[0] , $hobi[1] , $hobi[2]";
-?>
+
+echo "<br><br>=======================<br><br>";
+
+//OPERATOR
+$nilai1 = 5;
+$nilai2 = 8;
+$nilai3 = 10;
+$nilai4 = 20;
+$nilai5 = 13;
+$nilai6 = 0;
+
+$hasil = $nilai1 + $nilai2 - $nilai3 * $nilai4 /$nilai5;
+
+echo "hasil dari $nilai1 + $nilai2 - $nilai3 * $nilai4 /$nilai5 adalah $hasil";
+
+echo "<br><br>=======================<br><br>";
+//PERCABANGAN
+
+$nilai = 70;
+
+if($nilai >= 85){
+    echo "Grade A";
+} else if($nilai >= 70){
+    echo "Grade B";
+} else if($nilai >= 50){
+    echo "Grade C";
+} else if($nilai >= 30){
+    echo "Grade D";
+}else {
+    echo "Grade E";
+}
+?> 
+
